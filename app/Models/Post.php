72aa86380fec90f72bfef4c6cd131315a01e23c7
Eclatspad <eclatspad@gmail.com>
2025-03-20 21:04:52 +0000

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillables = ['user_id', 'where_it_happen', 'age', 'gender', 'rating', 'category', 'story'];
+}
